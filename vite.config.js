98086147cfd5a62dca846a0ef867b6c3e20141ac
sintiasnn <sintiawati.putu04@gmail.com>
2026-05-14 19:22:19 +0800

    feat: optimize images with sharp, fix hero text on mobile (left, smaller)

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,13 @@
 import { defineConfig } from 'vite';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
     base: '/web-sintiasgraphy-sintia/',
+    plugins: [
+        ViteImageOptimizer({
+            png: {
+                quality: 70,
+            },
+        }),
+    ],
 });
